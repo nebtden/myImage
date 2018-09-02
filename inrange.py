95b84c098fa56d0704b6148b43d918b4c2cbf208
simon.zhang <simon.zhang@kilimall.com>
2018-09-02 23:52:37 +0800

    后续测试

--- inrange.py
+++ inrange.py
@@ -1,26 +1,38 @@
 import numpy as np
 from pandas import *
 import cv2
+import colorsys
 
 img = cv2.imread('sources/mickey2.jpg')
 print(type(img))
-newimg = np.ones(img.shape)
-size = img.shape
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# 254,194,62  黄颜色
+# 252,25,34  黄颜色
 
 
-kernel = np.ones((10,10),np.uint8)
-erosion = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-a = img.reshape(size[0]*size[1],3)
-# print(a.shape)
-# # 对python进行分块处理
-b = DataFrame(a).drop_duplicates().values
-print( type(b))
-print(b.shape)
+lower_red = np.array([0,50,50])
+upper_red = np.array([10,255,255])
+mask0 = cv2.inRange(hsv, lower_red, upper_red)
 
-c = erosion.reshape(size[0]*size[1],3)
-# print(a.shape)
-# # 对python进行分块处理
-d = DataFrame(c).drop_duplicates().values
-print( type(d))
-print(d.shape)
+# upper mask (170-180)
+lower_red = np.array([170,50,50])
+upper_red = np.array([180,255,255])
+mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+lower_red = np.array([11, 50, 50])
+upper_red = np.array([34, 255, 255])
+mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+# join my masks
+red = mask0+mask1
+
+
+cv2.imshow('src',img)
+cv2.imshow('red',red)
+cv2.imshow('show2',mask2)
+# cv2.waitKey()
+while(1):
+    # cv2.imshow('newimg', newimg)
+    if cv2.waitKey(1) == ord('q'):
+        break
