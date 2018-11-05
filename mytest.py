e3a557eac2857042e065fda3d4fb28501932124d
nebtden <nebtden@google.com>
2018-11-05 17:35:29 +0800

    a

--- mytest.py
+++ mytest.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 
+from core.simple_color import  getcolorindex
 import numpy as np
 
-x = (1,1,1)
-y = (2,2,2)
-var = np.var([x,y])
-x = (1,1,1)
-y = (1,2,1)
-var = np.var([x,y])
-min = np.argmin([1,5,-1,4],axis=0)
-print(min)
+x = [4,5,6]
+index = getcolorindex(x)
+print(index)
